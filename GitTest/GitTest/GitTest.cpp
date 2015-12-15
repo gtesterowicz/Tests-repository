93bbc866ed97dfce62cf4c34e2db96e460be6351
gtesterowicz <aktywacje93@gmail.com>
2015-12-15 20:01:42 +0100

    initial commit

---
+++ GitTest/GitTest/GitTest.cpp
@@ -0,0 +1,20 @@
+// GitTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Git Test!" << endl;
+
+	getchar();
+	getchar();
+
+	return 0;
+}
+
